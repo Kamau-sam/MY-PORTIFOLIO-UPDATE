4793fb05a8169032e1d76bb80629180bb5edf9ce
Kamau-sam <skamau.6411@gmail.com>
2024-03-18 14:27:32 +0300

    my portifolio update

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Samuel Kamau's Portfolio!");
+function logHobbies() {
+    const hobbies = ['Cycling', 'Watching', 'Playing football', 'Gaming', 'Meditation'];
+    console.log('MY HOBBIES:');
+    hobbies.forEach(hobby => {
+        console.log(hobby);
+    });
+}
